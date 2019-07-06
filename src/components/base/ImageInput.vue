83a945015979ce5c6c8fc6add86cc0f518fc0101
Sallar <sallar.shayegan@gmail.com>
2019-07-06 11:24:38 +0200

    image crop and resize been improved

--- src/components/base/ImageInput.vue
+++ src/components/base/ImageInput.vue
@@ -3,30 +3,26 @@
     <div>
       <input type="file" name="image" @change="addFile($event)"/>
     </div>
-    <div class="canvas-container">
-      <div id="border" @mousedown="startDrag"
+    <div class="canvas-container" @mousemove="doResize" @mouseup="stopResize">
+      <div id="border" @mousedown.self="startDrag"
                        @mousemove="doDrag"
                        @mouseup="stopDrag"
                        :style="borderStyle">
-        <div class="top-container">
-          <div id="top-left"></div>
-          <div id="top"></div>
-          <div id="top-right"></div>
+        <div class="ne-container">
+          <div id="top-right" @mousedown="startResize($event, 'ne')"></div>
+          <div id="right-top" @mousedown="startResize($event, 'ne')"></div>
         </div>
-        <div class="bottom-container">
-          <div id="bottom-left"></div>
-          <div id="bottom"></div>
-          <div id="bottom-right"></div>
+        <div class="nw-container">
+          <div id="top-left" @mousedown="startResize($event, 'nw')"></div>
+          <div id="left-top" @mousedown="startResize($event, 'nw')"></div>
         </div>
-        <div class="left-container">
-          <div id="left-top"></div>
-          <div id="left"></div>
-          <div id="left-bottom"></div>
+        <div class="se-container">
+          <div id="bottom-right" @mousedown="startResize($event, 'se')"></div>
+          <div id="right-bottom" @mousedown="startResize($event, 'se')"></div>
         </div>
-        <div class="right-container">
-          <div id="right-top"></div>
-          <div id="right"></div>
-          <div id="right-bottom"></div>
+        <div class="sw-container">
+          <div id="bottom-left" @mousedown="startResize($event, 'sw')"></div>
+          <div id="left-bottom" @mousedown="startResize($event, 'sw')"></div>
         </div>
       </div>
       <canvas id="canvas"></canvas>
@@ -40,29 +36,72 @@ export default {
     return {
       image: null,
       dragging: false,
+      resizing: false,
       mouse: {
         startX: 0, startY: 0, x: 0, y: 0,
       },
       border: {
-        top: 0, left: 0, lastLeft: 0, lastTop: 0, size: 0,
+        top: 0, left: 0, lastLeft: 0, lastTop: 0, size: 0, lastSize: 0,
       },
       canvas: { width: 0, height: 0, src: '' },
     };
   },
   computed: {
     borderStyle() {
-      let top = this.border.lastTop + this.mouse.y - this.mouse.startY;
-      let left = this.border.lastLeft + this.mouse.x - this.mouse.startX;
+      let { top } = this.border;
+      let { left } = this.border;
+      let { size } = this.border;
+      const dx = this.mouse.x - this.mouse.startX;
+      const dy = this.mouse.y - this.mouse.startY;
+      const minDistance = ((dx < dy) ? dx : dy);
+
+      if (this.dragging) {
+        top = this.border.lastTop + dy;
+        left = this.border.lastLeft + dx;
+      } else if (this.resizing === 'ne') { // top-right resize
+        top = this.border.top + minDistance;
+        size = this.border.size - minDistance;
+      } else if (this.resizing === 'nw') { // top-left resize
+        top = this.border.top + minDistance;
+        left = this.border.left + minDistance;
+        size = this.border.size - minDistance;
+      } else if (this.resizing === 'se') { // bottom-right resize
+        size = this.border.size + minDistance;
+      } else if (this.resizing === 'sw') { // bottom-left resize
+        left = this.border.left + minDistance;
+        size = this.border.size - minDistance;
+      }
       if (top < 0) top = 0;
       if (left < 0) left = 0;
       if (top + this.border.size > this.canvas.height) top = this.canvas.height - this.border.size;
       if (left + this.border.size > this.canvas.width) left = this.canvas.width - this.border.size;
+      if (size > this.canvas.height) size = this.canvas.height;
+      if (size > this.canvas.width) size = this.canvas.width;
       return `top:${top}px;left:${left}px;`
-           + `width:${this.border.size}px;`
-           + `height:${this.border.size}px;`;
+           + `width:${size}px;`
+           + `height:${size}px;`;
     },
   },
   methods: {
+    startResize(event, type) {
+      this.border.lastSize = this.border.size;
+      this.resizing = type;
+      console.log(type);
+      this.mouse.startX = event.clientX;
+      this.mouse.startY = event.clientY;
+      this.mouse.x = event.clientX;
+      this.mouse.y = event.clientY;
+    },
+    doResize(event) {
+      if (this.resizing) {
+        this.mouse.x = event.clientX;
+        this.mouse.y = event.clientY;
+      }
+    },
+    stopResize() {
+      this.resizing = false;
+      this.border.size = this.getBorderSize();
+    },
     startDrag(event) {
       this.border.lastLeft = this.border.left;
       this.border.lastTop = this.border.top;
@@ -76,14 +115,14 @@ export default {
       if (this.dragging) {
         this.mouse.x = event.clientX;
         this.mouse.y = event.clientY;
-        this.border.left = this.getBorderX();
-        this.border.top = this.getBorderY();
       }
     },
     stopDrag() {
       this.dragging = false;
-      // Build cropped image:
+      this.border.left = this.getBorderX();
+      this.border.top = this.getBorderY();
 
+      // Build cropped image:
       const canvasFile = document.createElement('canvas');
       const jpeg = new Image();
 
@@ -91,7 +130,7 @@ export default {
         canvasFile.height = this.border.size;
         canvasFile.width = this.border.size;
         const canvasFile2 = canvasFile.getContext('2d');
-        canvasFile2.drawImage(jpeg, this.getBorderX(), this.getBorderY(),
+        canvasFile2.drawImage(jpeg, this.border.left, this.border.top,
           this.border.size, this.border.size,
           0, 0, this.border.size, this.border.size);
 
@@ -111,6 +150,10 @@ export default {
     getBorderY() {
       const { top } = document.getElementById('border').style;
       return parseInt(top.substring(0, top.indexOf('px')), 10);
+    },
+    getBorderSize() {
+      const { width } = document.getElementById('border').style;
+      return parseInt(width.substring(0, width.indexOf('px')), 10);
     },
     addFile(event) {
       const image = new Image();
@@ -145,63 +188,62 @@ export default {
   display: none;
 }
 #border {
-  width: 100px;
-  height: 100px;
   position: absolute;
+  border: 1px solid #ffffff;
   z-index: 1000;
   cursor: move;
-  box-shadow: 0px 0px 10000000px #ffffff;
+  box-shadow: 0px 0px 10000px #ffffff;
+  display: grid;
+  grid-template-columns: 30px auto 30px;
+  grid-template-rows: 30px auto 30px;
+  min-height: 60px;
+  min-width: 60px;
+}
+#border div {
   display: grid;
   grid-template-columns: 5px auto 5px;
   grid-template-rows: 5px auto 5px;
-  min-height: 20px;
 }
 #border div div {
-  background: #333333;
-  border: 1px solid #cccccc;
+  background: #000000;
+  opacity: 0.5;
 }
-.top-container {
-  grid-column: 2 / 3;
+.ne-container {
+  grid-column: 3;
   grid-row: 1;
 }
-.bottom-container {
-  grid-column: 2 / 3;
-  grid-row: 3;
-}
-.left-container {
-  grid-column: 3;
-  grid-row: 2 / 3;
-}
-.right-container {
+.nw-container {
   grid-column: 1;
-  grid-row: 2 / 3;
-}
-.top-container, .bottom-container {
-  display: grid;
-  grid-template-columns: 15% auto 20% auto 15%;
-}
-.left-container, .right-container {
-  display: grid;
-  grid-template-rows: 15% auto 20% auto 15%;
-}
-#top-left, #bottom-left {
-  grid-column: 1;
-}
-#top-right, #bottom-right {
-  grid-column: 5;
-}
-#left-top, #right-top {
   grid-row: 1;
 }
-#left-bottom, #right-bottom {
-  grid-row: 5;
-}
-#top, #bottom {
-  cursor: ns-resize;
+.se-container {
   grid-column: 3;
-}
-#left, #right {
-  cursor: ew-resize;
   grid-row: 3;
+}
+.sw-container {
+  grid-column: 1;
+  grid-row: 3;
+}
+#top-left, #top-right {
+  grid-column: 1 / 3;
+  grid-row: 1;
+}
+#bottom-left, #bottom-right {
+  grid-column: 1 / 3;
+  grid-row: 3;
+}
+#left-top, #left-bottom {
+  grid-column: 1;
+  grid-row: 1 / 3;
+}
+#right-top, #right-bottom {
+  grid-column: 3;
+  grid-row: 1 / 3;
+}
+#top-left, #left-top, #bottom-right, #right-bottom {
+  cursor: nwse-resize;
+}
+#top-right, #right-top, #bottom-left, #left-bottom {
+  cursor: nesw-resize;
 }
 </style>
